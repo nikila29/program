37330fbf28c2b29727b62ef2856c86c89e67dcbf
nikila29 <163622835+nikila29@users.noreply.github.com>
2024-03-26 15:46:10 +0530

    Add files via upload

---
+++ minimum.py
@@ -0,0 +1,14 @@
+import heapq
+def golden_wire(arr):
+    heapq.heapify(arr)
+    tot_cost=0
+    while len(arr) > 1:
+        w1=heapq.heappop(arr)
+        w2=heapq.heappop(arr)
+        tot_cost+=(w1+w2)
+        heapq.heappush(arr,(w1+w2))
+    return tot_cost
+
+arr=list(map(int,input("enter all wries length").split()))
+tot_cost=golden_wire(arr)
+print("total cost : ",tot_cost)
